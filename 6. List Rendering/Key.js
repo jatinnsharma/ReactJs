e7b0455f60799a1f79f94c0d6179f730890eb7ce
jatin <88488554+jatinnsharma@users.noreply.github.com>
2023-06-09 21:55:57 +0530

    Update Key.js

--- 6. List Rendering/Key.js
+++ 6. List Rendering/Key.js
@@ -2,7 +2,7 @@ import React from 'react'
 // rendering of List in React?
 // How map() works in React?
 const Key = () => {
-    const IPL=['RR','RCB','MI','RCB']
+    const IPL=['RR','RCB','MI','RCB','RCB']
     const result=IPL.map((ipl,index)=><h1 key={index}>{ipl} index is {index}</h1>)
   return (
     <div>
